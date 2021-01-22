66937078950b36b644f25430de41c0303dc915fd
SungHwan <encore@encore-solution.asia-northeast3-a.c.encore-product-proj.internal>
2021-01-22 15:18:10 +0900

    src_deploy.sh

---
+++ src_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOME_PATH=`pwd`
+SRC_PATH=$HOME_PATH/en_solution
+NOW_DATE=$(date +%Y%m%d)
+
+
+cd $SRC_PATH/20210118
+
+#command
+unzip dataware-module-meta-oracle-4.7.0.war -d ./ora_dataware
+mv 
